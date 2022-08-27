7b67e87bdc1d11e81c9e089d713b015d4f820d8b
kenicodex <kehindesalaudeen222@gmail.com>
2022-08-27 14:11:25 +0100

    product cards fix

--- src/components/ecommerce/Header/Header.jsx
+++ src/components/ecommerce/Header/Header.jsx
@@ -4,17 +4,17 @@ import './header.css'
 
 function Header() {
     return (
-        <Box className="bgimage" component={"section"} sx={{ height: { xs: "auto", lg: "500px" },backgroundImage:'url(https://images.unsplash.com/photo-1524508762098-fd966ffb6ef9?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80)' 
+        <Box className="bgimage" component={"section"} sx={{ height: { xs: "500px", lg: "500px" },backgroundImage:'url(https://images.unsplash.com/photo-1524508762098-fd966ffb6ef9?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80)' 
         }}>
-            <div className="container-fluid">
-                <div className="row text-center">
+            {/* <div className="container-fluid"> */}
+                <Box className="row text-center" sx={{display:"flex", justifyContent:"center", alignItems:"center"}}>
                     <Typography component={'div'} className=" text-white w-100" sx={{ flexDirection: "column", width: "100%" }}>
                         <h3 className='w-100'>Welcome to Kenny Inc Store</h3>
                         <Typography sx={{ py: { xs: "20px", lg: "70px" } }}>Start your purchases easy and cheap. </Typography>
                         <p><a href="/fakestore" className="btn btn-outline-primary btn-large px-4">Learn more </a></p>
                     </Typography>
-                </div>
-            </div>
+                </Box>
+            {/* </div> */}
         </Box>
     )
 }
--- src/components/ecommerce/Home/Home.jsx
+++ src/components/ecommerce/Home/Home.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import { Box, Typography } from "@mui/material";
+import { Box, Slide, Typography } from "@mui/material";
 import EcomNav from "../Navbar/EcomNav.jsx";
 import Footer from "../Navbar/Footer.jsx";
 import Header from "../Header/Header.jsx";
@@ -24,14 +24,14 @@ function Store(params) {
         <Box className="bg-light">
             <EcomNav />
             <Header />
-            <Box sx={{ height: "400px", display: 'flex', flexDirection: "row", justifyContent: "space-evenly", alignItems: "center" }} className="bg-white">
-                <Box sx={{ width: "48%", height: "370px", justifyContent: "center", alignItems: "center" }} className="d-flex" >
-                    <Typography fontSize={"40px"} fontWeight="600" component="div" sx={{ width: "80%" }}>
+            <Box sx={{ height: "400px", display: 'flex', flexDirection: {xs:"column",lg:"row"}, justifyContent: "space-evenly", alignItems: "center" }} className="bg-white">
+                <Box sx={{ width: {xs:"100%", lg:"48%"}, height: {xs:"auto",lg:"370px"}, justifyContent: "center", alignItems: "center" }} className="d-flex" >
+                    <Typography fontSize={{xs:"25px",lg:"40px"}} fontWeight="600" component="div" sx={{ width: {xs:"80%",lg:"80%"} }}>
                         With ease from your remote location make orders and purschase products
                     </Typography>
                 </Box>
-                <Box sx={{ width: "48%", height: "auto" }} className="" >
-                    <img src={process.env.PUBLIC_URL + '/images/ecom.webp'} width={"90%"} height={"auto"} alt="" />
+                <Box sx={{ width: {xs:"100%", lg:"48%"}, height: "auto" }} className="" >
+                    <Slide in={true} direction="right" timeout={2000}><img src={process.env.PUBLIC_URL + '/images/ecom.webp'} width={"90%"} height={"auto"} alt="" /></Slide>
                 </Box>
             </Box>
             <Box component={"div"} sx={{
@@ -40,12 +40,12 @@ function Store(params) {
             }}>
                 {product !== undefined ? product.map(({ title, thumbnail, price }, i) => {
                     return (
-                        <Box sx={{ width: { xs: "32.34%", md: "18.8%" }, height: "auto", my: "5px" }} className="border rounded bg-white text-center " key={i}>
-                            <img src={thumbnail} alt="" width="100%" height={"70%"} />
-                            <Box sx={{height:"30%"}}>
-                                {title} <br />
-                               <span className="text-primary"> ${price}</span> <br />
-                                <Typography className="btn btn-outline-primary w-75 mb-1 text-sm" fontSize={"12px"}> Add to Cart</Typography>
+                        <Box sx={{ width: { xs: "32.34%", md: "18.8%" }, height: "250px", my: "5px" }} className="border rounded bg-white text-center " key={i}>
+                            <img src={thumbnail} alt="" width="100%" style={{maxHeight:"70%", height:"70%", minHeight:"50%"}} />
+                            <Box sx={{maxHeight:"30%", height:"30%", minHeight:"30%",py:"5px"}} className="text-center">
+                                <Typography sx={{height:"30%"}} fontSize={"60%"}>{title} </Typography>
+                                <Typography sx={{height:"30%"}} fontSize={"60%"} fontWeight={"600"} className="text-danger"> ${price}</Typography>
+                                <Typography sx={{height:"30%", ml:"12.5%"}} className="btn-outline-primary w-75 border rounded " fontSize={"70%"}> Add to Cart</Typography>
                             </Box>
                         </Box>)
                 }) : <Box className="alert alert-danger text-center" component={"span"}> {message}</Box>}
