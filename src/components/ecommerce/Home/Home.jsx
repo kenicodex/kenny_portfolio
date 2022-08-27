55133bc0c1895c717cbc0cf8dcca300c93a68d44
kenicodex <kehindesalaudeen222@gmail.com>
2022-08-27 15:51:37 +0100

    redirects fix

--- src/components/ecommerce/Home/Home.jsx
+++ src/components/ecommerce/Home/Home.jsx
@@ -24,13 +24,13 @@ function Store(params) {
         <Box className="bg-light">
             <EcomNav />
             <Header />
-            <Box sx={{ height: "400px", display: 'flex', flexDirection: {xs:"column",lg:"row"}, justifyContent: "space-evenly", alignItems: "center" }} className="bg-white">
-                <Box sx={{ width: {xs:"100%", lg:"48%"}, height: {xs:"auto",lg:"370px"}, justifyContent: "center", alignItems: "center" }} className="d-flex" >
-                    <Typography fontSize={{xs:"25px",lg:"40px"}} fontWeight="600" component="div" sx={{ width: {xs:"80%",lg:"80%"} }}>
+            <Box sx={{ height: "400px", display: 'flex', flexDirection: { xs: "column", lg: "row" }, justifyContent: "space-evenly", alignItems: "center" }} className="bg-white">
+                <Box sx={{ width: { xs: "100%", lg: "48%" }, height: { xs: "auto", lg: "370px" }, justifyContent: "center", alignItems: "center" }} className="d-flex" >
+                    <Typography fontSize={{ xs: "25px", lg: "40px" }} fontWeight="600" fontFamily={"open sans"} component="div" sx={{ width: { xs: "80%", lg: "80%" } }} className="text-primary" >
                         With ease from your remote location make orders and purschase products
                     </Typography>
                 </Box>
-                <Box sx={{ width: {xs:"100%", lg:"48%"}, height: "auto" }} className="" >
+                <Box sx={{ width: { xs: "100%", lg: "48%" }, height: "auto" }} className="" >
                     <Slide in={true} direction="right" timeout={2000}><img src={process.env.PUBLIC_URL + '/images/ecom.webp'} width={"90%"} height={"auto"} alt="" /></Slide>
                 </Box>
             </Box>
@@ -40,12 +40,14 @@ function Store(params) {
             }}>
                 {product !== undefined ? product.map(({ title, thumbnail, price }, i) => {
                     return (
-                        <Box sx={{ width: { xs: "32.34%", md: "18.8%" }, height: "250px", my: "5px" }} className="border rounded bg-white text-center " key={i}>
-                            <img src={thumbnail} alt="" width="100%" style={{maxHeight:"70%", height:"70%", minHeight:"50%"}} />
-                            <Box sx={{maxHeight:"30%", height:"30%", minHeight:"30%",py:"5px"}} className="text-center">
-                                <Typography sx={{height:"30%"}} fontSize={"60%"}>{title} </Typography>
-                                <Typography sx={{height:"30%"}} fontSize={"60%"} fontWeight={"600"} className="text-danger"> ${price}</Typography>
-                                <Typography sx={{height:"30%", ml:"12.5%"}} className="btn-outline-primary w-75 border rounded " fontSize={"70%"}> Add to Cart</Typography>
+                        <Box sx={{ width: { xs: "32.34%", md: "18.8%" }, height: {xs:"150px",md:"250px"}, my: "5px" }} className="border rounded bg-light shadow text-center " key={i}>
+                            <Box sx={{ height: "70%", display:"flex", alignItems:"center" }}>
+                                <img src={thumbnail} alt="" width="100%" style={{ maxHeight: "100%", minHeight: "30%" }} />
+                            </Box>
+                            <Box sx={{ height: "30%",  }} className="text-center">
+                                <Typography sx={{ height: "30%" }} fontSize={{xs:"50%",lg:"70%"}} fontWeight={"600"} >{title} </Typography>
+                                <Typography sx={{ height: "30%" }} fontSize={{xs:"50%",lg:"70%"}} fontWeight={"600"} className="text-danger"> ${price}</Typography>
+                                <Typography sx={{ height: "30%", ml: "12.5%" }} className="btn-outline-primary w-75 border rounded " fontSize={{xs:"50%",lg:"70%"}}> Add to Cart</Typography>
                             </Box>
                         </Box>)
                 }) : <Box className="alert alert-danger text-center" component={"span"}> {message}</Box>}
